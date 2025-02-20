892ad4855c357962259d8de15d6c2939b283f047
Biel Costa <118078513+BielC19@users.noreply.github.com>
2025-02-20 07:38:46 +0100

    a

--- app/page.tsx
+++ app/page.tsx
@@ -11,48 +11,48 @@ export default function Home() {
     {
       title: `Motor d’Excitació Independent`,
       content: `Motor de corrent continu amb bobinatges separats, cosa que permet un control precís de la velocitat. És útil en aplicacions que requereixen estabilitat i resposta ràpida, com grues i trens elèctrics.`,
-      image: `https://picsum.photos/seed/2/800/400`,
+      image: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbBgqXaBgSIBVgFT-AUN8re4cbrul_TZdwjg&s`,
     },
     {
       title: `Motor d’Excitació en Sèrie`,
       content: `Motor amb bobinatge d’excitació en sèrie amb l’induït, que proporciona un alt parell d’arrencada però una velocitat que disminueix amb la càrrega. Ideal per tramvies, ascensors i muntacàrregues, però perillós sense càrrega.`,
-      image: `https://picsum.photos/seed/3/800/400`,
+      image: `https://www.sogears.com/images/demo/portfolio/ELECTRIC-MOTOR.jpg`,
     },
     {
       title: `Motor d’Excitació en Derivació (Shunt)`,
       content: `Motor amb bobinatge en paral·lel amb l’induït, fet que permet mantenir una velocitat constant. És eficient i estable en aplicacions com màquines industrials, bombes i ventiladors.`,
-      image: `https://picsum.photos/seed/4/800/400`,
+      image: `https://electrocentercol.com/wp-content/uploads/el-motor-de-derivacion-principios-de-funcionamiento-y-composicion-2.jpg`,
     },
     {
       title: `Motor d’Excitació Composta`,
       content: `Combina característiques dels motors en sèrie i en derivació, oferint un bon equilibri entre parell d’arrencada i estabilitat de velocitat. Utilitzat en tracció elèctrica, generadors i equips industrials.`,
-      image: `https://picsum.photos/seed/5/800/400`,
+      image: ``,
     },
   ]
   const cardData2 = [
     {
       title: `Motors Síncrons `,
       content: `Els motors síncrons funcionen a una velocitat constant sincronitzada amb la freqüència del corrent altern. A causa del seu funcionament, necessiten sistemes externs per a l’arrencada, com ara excitació addicional o variadors de freqüència, per assolir la velocitat de sincronització.`,
-      image: `https://picsum.photos/seed/1/800/400`,
+      image: ``,
     },
     {
       title: `Motors Asíncrons`,
       content: `Els motors asíncrons són els més utilitzats a la indústria per la seva simplicitat i resistència. Són resistents, econòmics i requereixen poc manteniment. Poden tenir un rotor de gàbia d'esquirol, que és el més comú per la seva robustesa i economia, o un rotor bobinat, que permet un millor control de l'arrencada. Són habituals en aplicacions com ventiladors, bombes d’aigua i refrigeradors.`,
-      image: `https://picsum.photos/seed/2/800/400`,
+      image: ``,
     },
   ]
   const cardData3 = [
     {
       title: `Motors Monofàsics i Universals`,
       content: `Els motors monofàsics es fan servir en habitatges i petits comerços. Els motors d'inducció amb bobinatge auxiliar tenen un bobinatge addicional per facilitar l’arrencada i, en alguns casos, un condensador per millorar el procés. Els motors de condensador poden tenir un condensador permanent, que funciona sempre, o un condensador d’arrencada, que es desconnecta després d’engegar-se. Aquests motors es troben en electrodomèstics com rentadores, aires condicionats i refrigeradors. Els motors universals poden funcionar tant amb corrent continu com altern, tenen una velocitat molt alta i s’utilitzen en aspiradores, trepants i liquadores.`,
-      image: `https://picsum.photos/seed/1/800/400`,
+      image: ``,
     },
   ]
   const cardData4 = [
     {
       title: `Motors Pas a Pas `,
       content: `Els motors pas a pas es mouen en petits passos en lloc de girar contínuament, fet que permet un control precís del moviment. Requereixen un controlador electrònic per al seu funcionament. Hi ha dos tipus principals: els de reluctància variable, on el rotor no té imants, només ferro, i els híbrids, que combinen reluctància i imants permanents per millorar la precisió. Són comuns en impressores, robots i maquinària de control numèric.`,
-      image: `https://picsum.photos/seed/1/800/400`,
+      image: ``,
     },
     
   ]
