b81c6afd0c86403efccb3ae3300f4895460fb77c
Biel Costa <118078513+BielC19@users.noreply.github.com>
2025-02-20 11:16:53 +0100

    catala

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['picsum.photos'],
-  },
-}
+import type { NextConfig } from "next";
 
-module.exports = nextConfig
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+export default nextConfig;
